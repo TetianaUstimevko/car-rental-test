48e656e823491bbfc298b07a2203aa0fb3baaf59
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-09-16 17:48:53 +0300

    add btn LoadMore

---
+++ src/components/BtnLoadMore/BtnLoadMore.jsx
@@ -0,0 +1,12 @@
+// const BtnLoadMore = ({ onClick }) => {
+// 	return (
+// 		<button
+// 			type="button"
+// 			onClick={onClick}
+// 		>
+// 			Load more
+// 		</button>
+// 	);
+// };
+
+// export default BtnLoadMore;
