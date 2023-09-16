5ebc482ea8ec15484d78248d4a83af5bf309dd22
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-09-16 15:44:40 +0300

    add redux

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { carsApi } from "./carsOperation";
+
+// export const store = configureStore({
+// 	reducer: {
+// 		[carsApi.reducerPath]: carsApi.reducer,
+// 	},
+// 	middleware: getDefaultMiddleware().concat(carsApi.middleware),
+// });
+
+// export const persistor = store;
